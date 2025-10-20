c415ab66ed65850c7c99c0d48dc64c116ccf0cf7
gelson-jr <gelson.j@sluno.ifsc.edu.br>
2025-10-20 19:53:14 -0300

    Estrutura do Projeto

---
+++ app/src/main/java/br/ifsc/locadora/Main.java
@@ -0,0 +1,7 @@
+package br.ifsc.locadora;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Sistema de Locadora de Jogos Digitais iniciado!");
+    }
+}
