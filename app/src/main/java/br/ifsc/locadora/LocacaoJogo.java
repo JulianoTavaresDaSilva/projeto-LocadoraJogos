7cfe2167d2f04270a4376244f04e64d32e368a45
gelson-jr <gelson.j@sluno.ifsc.edu.br>
2025-10-20 20:17:34 -0300

    Código de Usuario e Locação Jogo

---
+++ app/src/main/java/br/ifsc/locadora/LocacaoJogo.java
@@ -0,0 +1,30 @@
+package br.ifsc.locadora;
+
+import java.time.LocalDate;
+
+public class LocacaoJogo {
+    private int id;
+    private LocalDate dataInicio;
+    private double valorTotal;
+    private Usuario usuario;
+    private JogoPlataforma jogoPlataforma;
+    private TempoLocacaoJogo tempo;
+
+    public LocacaoJogo(int id, Usuario usuario, JogoPlataforma jogoPlataforma, TempoLocacaoJogo tempo) {
+        this.id = id;
+        this.usuario = usuario;
+        this.jogoPlataforma = jogoPlataforma;
+        this.tempo = tempo;
+        this.dataInicio = LocalDate.now();
+        this.valorTotal = tempo.getDias() * jogoPlataforma.getPrecoDiario();
+    }
+
+    public double getValorTotal() { return valorTotal; }
+
+    @Override
+    public String toString() {
+        return "Locação #" + id + " | " + usuario.getNome() + " alugou " +
+                jogoPlataforma.getJogo().getTitulo() + " por " + tempo.getDias() +
+                " dias | Valor: R$ " + valorTotal;
+    }
+}
